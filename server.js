6858988fe6db5ef06631a249ec0e31863ad3b8e8
PageBossBlog <151723753+PageBossBlog@users.noreply.github.com>
2023-12-30 17:56:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ const app = express();
 app.use(bodyParser.json({ limit: '50mb' })); // Increase the limit for JSON payload
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // Increase the limit for URL-encoded payload
 
-app.use(cors({ origin: ["http://localhost:3000", "https://test-node-sender.onrender.com", "https://premium-nodemailer.onrender.com", "https://node-sender-bb4y0.onrender.com", "https://node-sender-stillbroke.onrender.com"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000", "https://test-node-sender.onrender.com", "https://premium-nodemailer.onrender.com", "https://node-sender-bb4y0.onrender.com", "https://node-sender-stillbroke.onrender.com", "https://malaika-mailer.onrender.com"], credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.use("/nodemailer", senderRoute);
+
+// Handle all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use((error, req, res, next) => {
   const errorStatus = error.status || 500;
