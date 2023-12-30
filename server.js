ae5696f0596a7a22299a443bd21a421a797a6a02
PageBossBlog <151723753+PageBossBlog@users.noreply.github.com>
2023-12-30 18:17:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import senderRoute from "./routes/sender.route.js";
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-app.use(bodyParser.json({ limit: '50mb' })); // Increase the limit for JSON payload
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // Increase the limit for URL-encoded payload
 
-app.use(cors({ origin: ["http://localhost:3000", "https://test-node-sender.onrender.com", "https://premium-nodemailer.onrender.com", "https://node-sender-bb4y0.onrender.com", "https://node-sender-stillbroke.onrender.com", "https://malaika-mailer.onrender.com"], credentials: true }));
-
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors({ origin: ["http://localhost:3000", "https://test-node-sender.onrender.com", "https://premium-nodemailer.onrender.com", "https://node-sender-bb4y0.onrender.com", "https://node-sender-stillbroke.onrender.com"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Use your existing routes
 app.use("/nodemailer", senderRoute);
 
 // Handle all other routes
@@ -24,17 +28,19 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+// Error handling middleware
 app.use((error, req, res, next) => {
   const errorStatus = error.status || 500;
   const errorMessage = error.message || "Something went wrong!";
-
   return res.status(errorStatus).send(errorMessage);
 });
 
+// Default route
 app.use("/", (req, res) => {
-	res.send("Welcome to Nodemailer Sender!")
+  res.send("Welcome to Nodemailer Sender!");
 });
 
-app.listen(8800, () => {
-  console.log("Sender is running!");
+const port = process.env.PORT || 8800;
+app.listen(port, () => {
+  console.log(`Sender is running on port ${port}`);
 });
